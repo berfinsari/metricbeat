0c0889c5911369339ab5d5660bf24b0683fc63bd
berfinsari <berfinsari21@gmail.com>
2017-02-25 23:51:47 +0000

    lsbeat güncellendi

--- lsbeat/beater/lsbeat.go
+++ lsbeat/beater/lsbeat.go
@@ -3,19 +3,21 @@ package beater
 import (
 	"fmt"
 	"time"
+	"io/ioutil"
 
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/libbeat/publisher"
 
-	"github.com/berfinsari/lsbeat/config"
+	"github.com/berfinsari/metricbeat/lsbeat/config"
 )
 
 type Lsbeat struct {
 	done   chan struct{}
 	config config.Config
 	client publisher.Client
+	lastIndexTime time.Time
 }
 
 // Creates beater
@@ -32,31 +34,62 @@ func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
 	return bt, nil
 }
 
+
 func (bt *Lsbeat) Run(b *beat.Beat) error {
-	logp.Info("lsbeat is running! Hit CTRL-C to stop it.")
+    logp.Info("lsbeat is running! Hit CTRL-C to stop it.")
 
-	bt.client = b.Publisher.Connect()
-	ticker := time.NewTicker(bt.config.Period)
-	counter := 1
-	for {
-		select {
-		case <-bt.done:
-			return nil
-		case <-ticker.C:
-		}
+    bt.client = b.Publisher.Connect()
+    ticker := time.NewTicker(bt.config.Period)
+    counter := 1
+    for {
 
-		event := common.MapStr{
-			"@timestamp": common.Time(time.Now()),
-			"type":       b.Name,
-			"counter":    counter,
-		}
-		bt.client.PublishEvent(event)
-		logp.Info("Event sent")
-		counter++
-	}
+        select {
+        case <-bt.done:
+            return nil
+        case <-ticker.C:
+        }
+
+        bt.listDir(bt.config.Path, b.Name, true)   // call lsDir
+        bt.lastIndexTime = time.Now()               // mark Timestamp
+
+        logp.Info("Event sent")
+        counter++
+    }
+
 }
 
 func (bt *Lsbeat) Stop() {
 	bt.client.Close()
 	close(bt.done)
+}
+
+func (bt *Lsbeat) listDir(dirFile string, beatname string, init bool) {
+    files, _ := ioutil.ReadDir(dirFile)
+    for _, f := range files {
+        t := f.ModTime()
+        //fmt.Println(f.Name(), dirFile+"/"+f.Name(), f.IsDir(), t, f.Size())
+
+        event := common.MapStr{
+            "@timestamp": common.Time(time.Now()),
+            "type":       beatname,
+            "modtime":    common.Time(t),
+            "filename":   f.Name(),
+            "path":       dirFile + "/" + f.Name(),
+            "directory":  f.IsDir(),
+            "filesize":   f.Size(),
+        }
+        if init {
+            // index all files and directories on init
+            bt.client.PublishEvent(event) //elasticsearch index.
+        } else {
+            // Index only changed files since last run.
+            if t.After(bt.lastIndexTime) {
+                bt.client.PublishEvent(event) //elasticsearch index.
+            }
+        }
+
+        if f.IsDir() {
+            bt.listDir(dirFile+"/"+f.Name(), beatname, init)
+        }
+    }
 }
--- lsbeat/main.go
+++ lsbeat/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"os"
-
-	"github.com/elastic/beats/libbeat/beat"
-
-	"github.com/berfinsari/lsbeat/beater"
+	"fmt"
+	"io/ioutil"
 )
-
 func main() {
-	err := beat.Run("lsbeat", "", beater.New)
-	if err != nil {
-		os.Exit(1)
-	}
+    //apply run path "." without argument.
+    if len(os.Args) == 1 {
+        listDir(".")
+    } else {
+        listDir(os.Args[1])
+    }
 }
+
+func listDir(dirFile string) {
+    files, _ := ioutil.ReadDir(dirFile)
+    for _, f := range files {
+        t := f.ModTime()
+        fmt.Println(f.Name(), dirFile+"/"+f.Name(), f.IsDir(), t, f.Size())
+
+        if f.IsDir() {
+            listDir(dirFile + "/" + f.Name())
+        }
+    }
+}
+
